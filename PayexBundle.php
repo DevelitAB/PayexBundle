7cfd98027b8ce447abd393587f596a47a805ec32
Stas Bannikov <sab.ukraine@gmail.com>
2018-06-20 16:01:09 +0300

    Update PayexBundle.php

--- PayexBundle.php
+++ PayexBundle.php
@@ -5,7 +5,7 @@ namespace Bsadnu\PayexBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class GrandIDBundle extends Bundle
+class PayexBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
