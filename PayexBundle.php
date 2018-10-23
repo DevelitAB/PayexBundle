139e2ac2b2e64c824183cf7262147921ae34131a
Stas Bannikov <sab.ukraine@gmail.com>
2018-10-23 14:56:42 +0300

    Update PayexBundle.php

--- PayexBundle.php
+++ PayexBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Bsadnu\PayexBundle;
+namespace DevelitAB\PayexBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
